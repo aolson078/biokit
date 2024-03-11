566323d22f5220e63e62f1850e61edecd372368f
Alexander Olson <aolson078@gmail.com>
2024-03-11 11:24:58 -0400

    Wrote function for predicting secondary structures

--- bio_algos/secondary_structure.py
+++ bio_algos/secondary_structure.py
@@ -3,56 +3,98 @@ from Bio.Seq import Seq
 
 
 def amino_acid_composition(seq):
-	"""
-		Translates RNA to amino acids and calculates the relative amounts of different amino acids in the sequence
+    """
+        Translates RNA to amino acids and calculates the relative amounts of different amino acids in the sequence
 
-		:param seq: String representing the RNA sequence.
-		:return: Dictionary containing tuple with count of amino acids and their percentage of the total
-	"""
+        :param seq: String representing the RNA sequence.
+        :return: Dictionary containing tuple with count of amino acids and their percentage of the total
+    """
 
-	amino_acid_seq = Seq(seq).translate()
+    amino_acid_seq = Seq(seq).translate()
 
-	amino_acids = {acid: (amino_acid_seq.count(acid), amino_acid_seq.count(acid) / len(amino_acid_seq)) for acid in
-	               set(amino_acid_seq)}
-	return amino_acids
+    amino_acids = {acid: (amino_acid_seq.count(acid), amino_acid_seq.count(acid) / len(amino_acid_seq)) for acid in
+                   set(amino_acid_seq)}
+    return amino_acids
 
 
 def hydrophobicity(amino_acid_dict):
-	"""
-		Calculates hydrophobicity of amino acid sequence using Eisenberg Hydrophobicity scale
-		(Higher value == more hydrophobic)
+    """
+        Calculates hydrophobicity of amino acid sequence using Eisenberg Hydrophobicity scale
+        (Higher value == more hydrophobic)
 
-		:param amino_acid_dict: Dictionary containing count of amino acid
-		:return: Float score representing the hydrophobicity of the amino acid sequence
-	"""
-	Eisenberg_scale = {'A': 0.62, "C": 0.29, "D": -0.9, "E": -0.74, "F": 1.19, "G": 0.48, "H": -0.4,
-	                   "I": 1.38, "K": -1.50, "L": 1.06, "M": 0.64, "N": -0.78, "P": 0.12, "Q": -0.85,
-	                   "R": -2.53, "S": -.018, "T": -0.05, "V": 1.08, "W": 0.81, "Y": 0.26, "*": 0}
+        :param amino_acid_dict: Dictionary containing count and relative percentage of amino acid
+        :return: Float score representing the hydrophobicity of the amino acid sequence
+    """
+    Eisenberg_scale = {'A': 0.62, "C": 0.29, "D": -0.9, "E": -0.74, "F": 1.19, "G": 0.48, "H": -0.4,
+                       "I": 1.38, "K": -1.50, "L": 1.06, "M": 0.64, "N": -0.78, "P": 0.12, "Q": -0.85,
+                       "R": -2.53, "S": -.018, "T": -0.05, "V": 1.08, "W": 0.81, "Y": 0.26, "*": 0}
 
-	# Sum up hydrophobicity value for each amino acid
-	score = sum(Eisenberg_scale[acid] for acid in amino_acid_dict)
-	return score
+    # Sum up hydrophobicity value for each amino acid
+    score = sum(Eisenberg_scale[acid] for acid in amino_acid_dict)
+    return score
 
+
+def ss_propensity(amino_acid_dict):
+    """
+        Calculates score based on each amino acids propensity to form each secondary structure
+        (This function only considers proteins and their α-Helix ands β-Sheet propensities)
+
+        :param amino_acid_dict: Dictionary containing count and relative percentage of amino acid
+        :return: String value of most likely secondary structures formed by the amino acid sequence
+    """
+    a_score = 0
+    b_score = 0
+
+    # Propensities for α-Helix (high + 5, medium + 3, low + 1)
+    a_high = ['A', 'E', 'K', 'L', 'M']
+    a_medium = ['H', 'I']
+    a_low = ['P']
+
+    # Propensities for β-Sheet (high + 5, medium + 3, low + 1)
+    b_high = ['V', 'W', 'Y']
+    b_medium = ['C', 'D', 'Q']
+    b_low = ['G', 'R', 'S', 'T']
+
+    for acid in amino_acid_dict.keys():
+        if acid in a_high:
+            a_score += 5
+        elif acid in a_medium:
+            a_score += 3
+        elif acid in a_low:
+            a_score += 1
+        elif acid in b_high:
+            b_score += 5
+        elif acid in b_medium:
+            b_score += 3
+        elif acid in b_low:
+            b_score += 1
+
+    prediction = 'α-Helix' if max(a_score, b_score) == a_score else 'β-Sheet'
+    return prediction
 
 # A sequence profile represents the distribution of specific properties (e.g., hydrophobicity, secondary structure propensity) along a protein sequence.
-def sequence_profiles(seq):
-	pass
+def sequence_profile(seq):
+    pass
 
 
 rna_sequence = dna_to_rna(
-	"GAAGCTGGACAGAGCCGGTTCCTGGAAAGAGCTGGTTCCCTGGCAGGCTGGAGGGCAGGAGCTGGGGCCACGCTGGTCTGGGATAGTTGGGCAGGGAGACGGAGTCTCGAT"
-	"CTGTCACCCAGGCTGGAGTGCAGTGGCACAACCTTGGCTCACTGCAACCTCCGCCTCCCAGGTTCAAGTGATTCTCCTGCCTCAGCCTTCTGAGTAGCTGGAATTACAAGC"
-	"TGTCTACCTGGTCTCCAGAATGGACGGCCCTGTGGCAGAGCATGCCAAGCAGGAGCCCTTTCACGTGGTCACACCTCTGTTGGAGAGCTGGGCGCTGTCCCAGGTGGCGGG"
-	"CATGCCTGTCTTCCTCAAGTGTGAGAATGTGCAGCCCAGCGGCTCCTTCAAGATTCGGGGCATTGGGCATTTCTGCCAGGAGATGGCCAAGAAGGGATGCAGACACCTGGT"
-	"GTGCTCCTCAGGGGGTAATGCGGGCATCGCTGCTGCCTATGCTGCTAGGAAGCTGGGCATTCCTGCCACCATCGTGCTCCCCGAGAGCACCTCCCTGCAGGTGGTGCAGAG"
-	"GCTGCAGGGGGAGGGGGCCGAGGTTCAGCTGACTGGAAAGGTCTGGGACGAGGCCAATCTGAGGGCGCAAGAGTTGGCCAAGAGGGACGGCTGGGAGAATGTCCCCCCGTT"
-	"TGACCACCCCCTAATATGGAAAGGCCACGCCAGCCTGGTGCAGGAGCTGAAAGCAGTGCTGAGGACCCCACCAGGTGCCCTGGTGCTGGCAGTTGGGGGTGGGGGTCTCCT"
-	"GGCCGGGGTGGTGGCTGGCCTGCTGGAGGTGGGCTGGCAGCATGTACCCATCATTGCCATGGAGACCCATGGGGCACACTGCTTCAATGCGGCCATCACAGCCGGCAAGCT"
-	"GGTCACACTTCCAGACATCACCAGTGTGGCCAAGAGCCTGGGTGCCAAGACGGTGGCCGCTCGGGCCCTGGAGTGCATGCAGGTGTGCAAGATTCACTCTGAAGTGGTGGA"
-	"GGACACCGAGGCTGTGAGCGCTGTGCAGCAGCTCCTGGATGATGAGCGTATGCTGGTGGAGCCTGCCTGTGGGGCAGCCTTAGCAGCCATCTACTCAGGCCTCCTGCGGAG"
-	"GCTCCAGGCCGAGGGCTGCCTGCCCCCTTCCCTGACTTCAGTTGTGGTAATCGTGTGTGGAGGCAACAACATCAACAGCCGAGAGCTGCAGGCTTTGAAAACCCACCTGGG"
-	"CCAGGTCTGAGGGGTCCCATCCTGGCCCCAAAGACCCCTGAGAGGCCCATGGACAGTCCTGTGTCTGGATGAGGAGGACTCAGTGCTGGCAGATGGCAGTGGAAGCTGCCC"
-	"TGTGCAACTGTGCTGGCTGCCTCCTGAAGGAAGCCCTCCTGGACTGCTTCTTTTGGCTCTCCGACAACTCCGGCCAATAAACACTTTCTGAATTGA")
+    "GAAGCTGGACAGAGCCGGTTCCTGGAAAGAGCTGGTTCCCTGGCAGGCTGGAGGGCAGGAGCTGGGGCCACGCTGGTCTGGGATAGTTGGGCAGGGAGACGGAGTCTCGAT"
+    "CTGTCACCCAGGCTGGAGTGCAGTGGCACAACCTTGGCTCACTGCAACCTCCGCCTCCCAGGTTCAAGTGATTCTCCTGCCTCAGCCTTCTGAGTAGCTGGAATTACAAGC"
+    "TGTCTACCTGGTCTCCAGAATGGACGGCCCTGTGGCAGAGCATGCCAAGCAGGAGCCCTTTCACGTGGTCACACCTCTGTTGGAGAGCTGGGCGCTGTCCCAGGTGGCGGG"
+    "CATGCCTGTCTTCCTCAAGTGTGAGAATGTGCAGCCCAGCGGCTCCTTCAAGATTCGGGGCATTGGGCATTTCTGCCAGGAGATGGCCAAGAAGGGATGCAGACACCTGGT"
+    "GTGCTCCTCAGGGGGTAATGCGGGCATCGCTGCTGCCTATGCTGCTAGGAAGCTGGGCATTCCTGCCACCATCGTGCTCCCCGAGAGCACCTCCCTGCAGGTGGTGCAGAG"
+    "GCTGCAGGGGGAGGGGGCCGAGGTTCAGCTGACTGGAAAGGTCTGGGACGAGGCCAATCTGAGGGCGCAAGAGTTGGCCAAGAGGGACGGCTGGGAGAATGTCCCCCCGTT"
+    "TGACCACCCCCTAATATGGAAAGGCCACGCCAGCCTGGTGCAGGAGCTGAAAGCAGTGCTGAGGACCCCACCAGGTGCCCTGGTGCTGGCAGTTGGGGGTGGGGGTCTCCT"
+    "GGCCGGGGTGGTGGCTGGCCTGCTGGAGGTGGGCTGGCAGCATGTACCCATCATTGCCATGGAGACCCATGGGGCACACTGCTTCAATGCGGCCATCACAGCCGGCAAGCT"
+    "GGTCACACTTCCAGACATCACCAGTGTGGCCAAGAGCCTGGGTGCCAAGACGGTGGCCGCTCGGGCCCTGGAGTGCATGCAGGTGTGCAAGATTCACTCTGAAGTGGTGGA"
+    "GGACACCGAGGCTGTGAGCGCTGTGCAGCAGCTCCTGGATGATGAGCGTATGCTGGTGGAGCCTGCCTGTGGGGCAGCCTTAGCAGCCATCTACTCAGGCCTCCTGCGGAG"
+    "GCTCCAGGCCGAGGGCTGCCTGCCCCCTTCCCTGACTTCAGTTGTGGTAATCGTGTGTGGAGGCAACAACATCAACAGCCGAGAGCTGCAGGCTTTGAAAACCCACCTGGG"
+    "CCAGGTCTGAGGGGTCCCATCCTGGCCCCAAAGACCCCTGAGAGGCCCATGGACAGTCCTGTGTCTGGATGAGGAGGACTCAGTGCTGGCAGATGGCAGTGGAAGCTGCCC"
+    "TGTGCAACTGTGCTGGCTGCCTCCTGAAGGAAGCCCTCCTGGACTGCTTCTTTTGGCTCTCCGACAACTCCGGCCAATAAACACTTTCTGAATTGA")
 
 amino_acid_dict = amino_acid_composition(rna_sequence)
 hydrophobicity_score = hydrophobicity(amino_acid_dict)
+print(amino_acid_dict)
+print(hydrophobicity_score)
+print(ss_propensity(amino_acid_dict))
+sequence_profile(rna_sequence)
