0a7ab8791c0aae8e4950c8865e1b0d78b0e6f0d6
Alex <aolson078@gmail.com>
2025-06-18 12:29:00 -0400

    refactored stacked_bar_chart.py

--- bio_algos/stacked_bar_chart.py
+++ bio_algos/stacked_bar_chart.py
@@ -1,47 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def count_nucs(nuc_string):
-	"""
-        Counts the number of nucleotides in a nucleotide string
-
-        :param nuc_string: string to count nucleotides
-        :return: dictionary object with nucleotides as keys and its count as values.
     """
-	nuc_counts = {'G': 0, 'A': 0, 'T': 0, 'C': 0}
-	for nuc in nuc_string:
-		if nuc in nuc_counts:
-			nuc_counts[nuc] += 1
-	return nuc_counts
+    Counts the number of nucleotides in a nucleotide string
 
-
-def stacked_bar_chart(nuc_list, organisms, output):
-	"""
-        Creates a bar chart with multiple stacked values using matplotlib and nucleotide counts
-
-        :param nuc_list: list containing strings of nucleotides to chart
-        :param organisms: list containing names of organisms being charted
-        :param output: output path to save file to
+    :param nuc_string: string to count nucleotides
+    :return: dictionary object with nucleotides as keys and their counts as values.
     """
-	x = ['G', 'A', 'T', 'C']
-	y_data = [count_nucs(nuc_string) for nuc_string in nuc_list]
-	bottom = np.zeros(4)
-	colors = plt.cm.viridis(np.linspace(0, 1, len(nuc_list)))
+    nucs = ['G', 'A', 'T', 'C']
+    nuc_counts = {nuc: 0 for nuc in nucs}
+    for nuc in nuc_string.upper():
+        if nuc in nuc_counts:
+            nuc_counts[nuc] += 1
+    return nuc_counts
 
-	fig, ax = plt.subplots(figsize=(10, 8))
+def grouped_bar_chart(nuc_list, organisms, output=None, show=False):
+    """
+    Creates a grouped bar chart of nucleotide counts for different organisms.
 
-	for i, y in enumerate(y_data):
-		plt.bar(x, list(y.values()), bottom=bottom, color=colors[i])
-		bottom += np.array(list(y.values()))
+    :param nuc_list: List of nucleotide strings for each organism.
+    :param organisms: List of organism names, same order as nuc_list.
+    :param output: Optional path to save the plot.
+    :param show: If True, displays the plot window.
+    """
+    nucs = ['G', 'A', 'T', 'C']
+    data = np.array([[count_nucs(nuc_string)[nuc] for nuc in nucs] for nuc_string in nuc_list])
 
-	ax.set_xlabel('Nucleotides')
-	ax.set_ylabel('Frequency')
-	ax.set_title('Stacked Bar Graph - Nucleotide Frequencies')
-	plt.legend(organisms, loc='upper right')
+    x = np.arange(len(nucs))
+    width = 0.8 / len(organisms)  # width of each bar
 
-	plt.tight_layout()
+    fig, ax = plt.subplots(figsize=(10, 7))
+    colors = plt.cm.viridis(np.linspace(0, 1, len(organisms)))
 
-	# save the figure as a PNG image file
-	plt.savefig(output, format='png', dpi=300, bbox_inches='tight')
-	plt.close(fig)
+    for i, (org, counts) in enumerate(zip(organisms, data)):
+        ax.bar(x + i*width, counts, width, label=org, color=colors[i])
+
+    ax.set_xticks(x + width * (len(organisms)-1) / 2)
+    ax.set_xticklabels(nucs)
+    ax.set_xlabel('Nucleotide')
+    ax.set_ylabel('Count')
+    ax.set_title('Nucleotide Frequency per Organism')
+    ax.legend(title='Organism')
+
+    plt.tight_layout()
+
+    if output:
+        plt.savefig(output, format='png', dpi=300, bbox_inches='tight')
+    if show:
+        plt.show()
+    plt.close(fig)
+
+# Example usage:
+nuc_list = ['GATTTGCA', 'GATTACA', 'GCGCGCAT']
+organisms = ['Human', 'Mouse', 'Yeast']
+grouped_bar_chart(nuc_list, organisms, output='nuc_freq.png', show=True)
